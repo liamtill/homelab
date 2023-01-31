3eead06e66e94d262f6695a5d381ed5ce52f972f
Liam Till <liam.till@live.co.uk>
2023-01-31 19:10:45 +0000

    add postgres ct

---
+++ terraform/postgres-ct/main.tf
@@ -0,0 +1,45 @@
+resource "proxmox_lxc" "postgres-server" {
+
+    # pve node
+    target_node = "nimbus-pve"
+
+    # define machine name
+    hostname = "postgres"
+    description = "PostgreSQL Server"
+    # template is unprivileged so no need to set option
+    ostemplate = "local:vztmpl/ubuntu-20.04-standard_20.04-1_amd64.tar.gz"
+    unprivileged = "true"
+    password = var.container_password
+    ssh_public_keys = var.ssh_public_key
+
+    # ct id to clone
+    # clone = "6000"
+    # full clone
+    # full = "true"
+    # start after creation
+    start = "true"
+    # start on boot
+    onboot = "true"
+
+    # ct settings
+    # clone_storage = "local-lvm"
+    cores = 2
+    memory = 2048
+    #swap = 2048
+
+    # define network
+    network {
+      bridge = "vmbr0"
+      name = "eth0"
+      ip = "dhcp"
+    }
+
+    # define root fs
+    rootfs {
+      storage = "local-lvm"
+      size    = "64G"
+    }
+
+
+}
+
