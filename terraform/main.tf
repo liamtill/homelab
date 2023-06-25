a8caaf0dae8c3156dc07b8f6804a637b6da01078
Liam Till <liam.till@live.co.uk>
2023-06-25 18:03:30 +0100

    moved media vm to use module

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,3 +2,8 @@ moved {
   from = proxmox_vm_qemu.docker
   to   = module.docker.proxmox_vm_qemu.proxmox-vm[0]
 }
+
+moved {
+  from = proxmox_vm_qemu.media
+  to = module.media.proxmox_vm_qemu.proxmox-vm[0]
+}
--- terraform/media-server.tf
+++ terraform/media-server.tf
@@ -1,59 +1,18 @@
-resource "proxmox_vm_qemu" "media" {
+module "media" {
+  source = "./modules/proxmox-vm"
 
-    # pve node
-    target_node = "nimbus-pve"
+  proxmox_api_url = var.proxmox_api_url
+  proxmox_api_token_id = var.proxmox_api_token_id
+  proxmox_user = var.proxmox_user
+  proxmox_user_password = var.proxmox_user_password
+  proxmox_api_token_secret = var.proxmox_api_token_secret
 
-    # define machine name
-    name = "media"
-    desc = "Media Server"
-    
-    # enable the qemu-guest-agent
-    agent = 1
+  vm_name = "media"
+  vm_description = "Media Server"
 
-    # vm template to clone from
-    clone = var.vm_template
-    # full clone instead of linked clone
-    full_clone  = "true"
+  num_cores = 4
+  memory = 4096
 
-    # vm settings
-    os_type = "cloud-init"
-    cores = 4
-    sockets = 1
-    cpu = "host"
-    memory = 4096
-    scsihw = "virtio-scsi-pci"
-    bootdisk = "scsi0"
-    boot = "cdn"
-    onboot = "true"
-
-    ipconfig0 = "ip=dhcp,ip6=dhcp"
-
-    # define network hw
-    network {
-      bridge = "vmbr0"
-      model = "virtio"
-    }
-
-    # define storage
-    disk {
-        storage = "local-lvm"
-        type = "scsi"
-        size = "32G"
-        # enable ssd emulation
-        ssd = 1
-    }
-
-    lifecycle {
-    ignore_changes  = [
-      network,
-    ]
-  }
-
-
-  # local commands, will use to run ansible plaaybooks for config
-  #provisioner "local-exec" {
-    #command = ""
-  #}
+  disk_size = "32G"
 
 }
-
