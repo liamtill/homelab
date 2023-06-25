92f3330018f64bfa26690fae5647c6a75fc79cf2
Liam Till <liam.till@live.co.uk>
2023-06-25 18:11:59 +0100

    moved postgres to use module

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,3 +12,8 @@ moved {
   from = proxmox_lxc.mysql-server
   to = module.mysql-server.proxmox_lxc.proxmox-ct[0]
 }
+
+moved {
+  from = proxmox_lxc.postgres-server
+  to = module.postgres.proxmox_lxc.proxmox-ct[0]
+}
